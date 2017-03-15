60f5b908ddca4ac788ef492faa98e2f4bc37b88a
Cesar Lopez <cesarl@outlook.com>
2017-03-15 13:30:26 -0500

    Adding Account Detail, List and Summary as well as routes in app.module.ts

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { UniversalModule } from 'angular2-universal';
 import { AppComponent } from './components/app/app.component'
-import { HomeComponent } from './components/home/home.component';
+import { AccountListComponent } from './components/account/account-list/account-list.component'
+import { AccountSummaryComponent } from './components/account/account-summary/account-summary.component'
+import { AccountDetailComponent } from './components/account/account-detail/account-detail.component'
 import { HeaderComponent } from './components/shared/header/header.component'
 
 
@@ -10,15 +12,18 @@ import { HeaderComponent } from './components/shared/header/header.component'
     bootstrap: [ AppComponent ],
     declarations: [
         AppComponent,
-        HomeComponent,
+        AccountListComponent,
+        AccountDetailComponent,
+        AccountSummaryComponent,
         HeaderComponent
     ],
     imports: [
         UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
         RouterModule.forRoot([
-            { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'home', component: HomeComponent },            
-            { path: '**', redirectTo: 'home' }
+            { path: '', redirectTo: 'account', pathMatch: 'full' },
+            { path: 'account', component: AccountListComponent },
+            { path: 'detail/:id', component: AccountDetailComponent },
+            { path: '**', redirectTo: 'account' }
         ])
     ]
 })
---
+++ ClientApp/app/components/account/account-summary/account-summary.component.ts
@@ -0,0 +1,11 @@
+﻿
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'account-summary',
+    templateUrl: './account-summary.component.html'
+})
+export class AccountSummaryComponent {
+    constructor() {
+    }
+}
